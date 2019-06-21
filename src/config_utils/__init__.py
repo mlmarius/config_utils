645c1af0eb716298d36969d2a0110ff3f19404c2
Liviu Manea <mlmarius@yahoo.com>
2019-06-22 00:45:31 +0300

    tests passing

--- src/config_utils/__init__.py
+++ src/config_utils/__init__.py
@@ -1,46 +1,159 @@
 from __future__ import annotations
 
+import configparser
 import os
-from typing import List, Callable, Dict, Union
+from abc import ABC, abstractmethod
+from typing import List, Callable, Dict, Union, Tuple, Any, Set
 
 
 class ConfigOption:
 
-    def __init__(self, name: str, default=None, value=None, processor: Callable = None, env_prefix: str = None,
+    def __init__(self, name: str, default=None, value=None, processor: Callable = None, section: str = None,
                  description: str = None):
         self.name = name
         self._processor = processor or (lambda x: x)
-        self._default = default
-        self._value = value
-        self.env_prefix = env_prefix
+        self.default = default
+        self.section = section
+        self.value = value
         self.description = description
 
-    @property
-    def env_name(self):
-        if self.env_prefix:
-            return self.env_prefix.upper() + self.name.upper()
-        return self.name.upper()
+    def process(self, value):
+        return self._processor(value)
+
+    def __hash__(self):
+        return hash((self.name, self.section))
+
+    def __eq__(self, other: ConfigOption):
+        return self.section == other.section and self.name == other.name
 
     def __str__(self):
-        return f"{self.name} - {self.description}"
+        return self.name
+
+
+class BaseConfig(ABC):
+
+    def __init__(self):
+        self.readers = []
+
+    @abstractmethod
+    def __getitem__(self, searched: Union[Tuple[str, str], ConfigOption]) -> Any:
+        pass
+
+    @abstractmethod
+    def options(self) -> Set[ConfigOption]:
+        pass
+
+
+class Config(BaseConfig):
+    def __init__(self, options: List[ConfigOption], readers: List[BaseConfig] = None):
+        self._options: Set[ConfigOption] = set(options) or set()
+        self.readers = readers or []
 
     @property
-    def value(self):
-        if self._value is not None:
-            return self._processor(self._value)
-        if self.env_name is not None:
+    def options(self) -> Set[ConfigOption]:
+        output = set() ^ self._options
+        for reader in self.readers:
+            output ^= reader.options
+        return output
+
+    def __add__(self, other: BaseConfig):
+        return Config(
+            options=self.options ^ other.options,
+            readers=self.readers + other.readers
+        )
+        # self.readers.append(other.readers)
+
+    def get_option(self, name: str, section: str = None) -> ConfigOption:
+        for option in self.options:
+            if option.name == name and option.section == section:
+                return option
+        else:
+            raise ValueError(f'Undefined option {name}')
+
+    def __getitem__(self, item: Union[str, Tuple[str, str], ConfigOption]) -> Any:
+
+        if not isinstance(item, ConfigOption):
+            if isinstance(item, str):
+                item = self.get_option(item, None)
+            elif isinstance(item, tuple):
+                name, section = item
+                item = self.get_option(name, section)
+
+        if item.value is not None:
+            return item.value
+
+        for reader in self.readers:
             try:
-                return self._processor(os.environ[self.env_name])
+                return item.process(reader[item])
+            except ConfigError:
+                continue
+
+        if item.default is not None:
+            return item.process(item.default)
+
+        raise ValueError(f'Undefined value {item.name}')
+
+    def cache(self) -> Dict[str, Union[str, bool, int, float]]:
+        # @todo return a result that can also include sections
+        output = {}
+        for option in self.options:
+            output[option.name] = option.value
+        return output
+
+
+class ConfigError(Exception):
+    pass
+
+
+class BaseConfigReader(BaseConfig):
+    @property
+    def options(self) -> Set[ConfigOption]:
+        return set()
+
+
+class EnvConfigReader(BaseConfigReader):
+
+    def __init__(self, prefix=None):
+        super().__init__()
+        self._prefix = prefix or ''
+
+    def _env_name(self, name: str) -> str:
+        return (self._prefix + name).upper()
+
+    def __getitem__(self, searched: ConfigOption) -> Any:
+        try:
+            return os.environ[self._env_name(searched.name)]
+        except KeyError:
+            raise ConfigError()
+
+
+class IniConfigReader(BaseConfigReader):
+    def __init__(self, filepath: str, section: str = None, sections: List[str] = None):
+        with open(filepath, 'rt') as f:
+            self._config = configparser.ConfigParser()
+            self._config.read_file(f)
+
+        if sections is not None:
+            self._sections = sections
+        elif section is not None:
+            self._sections = [section]
+        else:
+            raise Exception('Need to configure ONLY one of "section" or "sections"')
+
+    def __getitem__(self, searched: ConfigOption):
+        for section in self._sections:
+            try:
+                return self._config[section][searched.name]
             except KeyError:
-                if self._default is not None:
-                    return self._processor(self._default)
-        raise ValueError(f"Could not get value of {self.name}")
+                continue
+        else:
+            raise ConfigError()
 
 
-class Config:
-
-    def __init__(self, options: List[ConfigOption] = None, env_prefix=None):
+class Configy:
+    def __init__(self, options: List[ConfigOption] = None, env_prefix=None, fallback: Config = None):
         self.env_prefix = env_prefix or ''
+        self._fallback = fallback
         self.options = {}
         if options is not None:
             for option in options:
@@ -60,7 +173,9 @@ class Config:
 
     def __getitem__(self, item):
         if item not in self:
-            raise ValueError(f"Option {item} is not configured")
+            if self._fallback is None:
+                raise ValueError(f"Option {item} is not configured")
+            return self._fallback[item]
         return self.options.get(item).value
 
     def cache(self) -> Dict[str, Union[str, bool, int, float]]:
--- tests/test_one.py
+++ tests/test_one.py
@@ -1,48 +1,93 @@
+import os
+
 import pytest
 
-from config_utils import Config, ConfigOption
+from config_utils import Config, EnvConfigReader, IniConfigReader, ConfigOption
 
 
-def test_simple():
-    config = Config([
-        ConfigOption('OPTION1', 2),
-        ConfigOption('OPTION2', 15),
-        ConfigOption('OPTION3'),
-    ])
-    assert config['OPTION1'] is 2
-    assert config.options['OPTION1'].env_prefix == None
-    assert config.options['OPTION1'].env_name == 'OPTION1'
+def test_one():
+    os.environ['OPTION2'] = '33'
+    os.environ['OPTION3'] = 'spam'
 
-    assert config['OPTION2'] == 15
+    config1 = Config(
+        options=[
+            # Option with a default value. Found nowhere else
+            ConfigOption('option1', 1),
 
-    # should raise an exception when trying to access options that
-    # have no value, no defaults and not env variables defining them
+            # Option with a specified value, overriden in environment.
+            # Should return specified balue
+            ConfigOption('option2', value=2, processor=int),
+
+            # this one has a default value and an environment value
+            # it should return the environment value
+            ConfigOption('option3', 3)
+        ],
+        readers=[
+            EnvConfigReader()
+        ]
+    )
+
+    assert config1['option1'] == 1, 'You only had 1 job: return the default value of the option'
+    assert config1['option2'] == 2, 'This item had a hardcoded value. Where is it?'
+    assert config1['option3'] == 'spam', 'We should have received the environment value'
+
+    config2 = Config(
+        options=[
+            # we are overwriting a config option from the previous config
+            ConfigOption('option3', value='cat'),
+            ConfigOption('option4', 4)
+        ],
+        readers=[
+            IniConfigReader('tests/config.ini', sections=['bitbucket.org', 'topsecret.server.com'])
+        ]
+    )
+
+    # we are merging 2 configs
+    # if an option is defined in both
+    # the config2 will overwrite config1's option
+    config3 = config1 + config2
+
+    assert config2['option3'] == 'cat', 'This option should have been overridden after the merge'
+    assert config3['option4'] == 4
+
+    # should raise value error because we tried to access an option
+    # that is not defined in the config
     with pytest.raises(ValueError):
-        config['OPTION3']
+        assert config3['User'] == 'hg'
 
-    config = Config([
-        ConfigOption('OPTION1', 2),
-        ConfigOption('OPTION2', 15),
+    # make new config defining the User option
+    config4 = config3 + Config([
+        ConfigOption('User'),
+
+        # test if configparser picks up stuff in the DEFAULT
+        # config section
+        ConfigOption('ForwardX11'),
+
+        # define an option that is in the second section
+        # from our scanned sections list
+        ConfigOption('Port'),
+
+        ConfigOption('Undefined')
     ])
 
-    assert config.cache() == {'OPTION1': 2, 'OPTION2': 15}
+    assert config4['User'] == 'hg'
+
+    # Carefull! Even if you define searching in multiple sections,
+    # once a value is not found in the first section, then it will
+    # be searched in the DEFAULT section
+    assert config4['ForwardX11'] == 'yes'
+
+    # This item is found in our second searched list.
+    # It will only be returned if the first section does not have it
+    # AND if it is not defined in the DEFAULT section
+    assert config4['Port'] == '50022'
+
+    # We raise ValueError if the option is defined
+    # but we can't find its value
+    with pytest.raises(ValueError):
+        assert config4['Undefined']
 
 
-def test_with_prefix():
-    config1 = Config([
-        ConfigOption('OPTION1', 2),
-        ConfigOption('OPTION2', 15),
-    ], env_prefix='APP_')
-
-    assert config1.env_prefix == 'APP_'
-    assert config1.options['OPTION1'].env_prefix == 'APP_'
-    assert config1.options['OPTION1'].env_name == 'APP_OPTION1'
-
-    config2 = Config([
-        ConfigOption('OPTION2', 20),
-        ConfigOption('OPTION3', 30),
-    ], env_prefix='APP1_')
-
-    config_total = config1 + config2
-
-    assert config_total.cache() == {'OPTION1': 2, 'OPTION2': 20, 'OPTION3': 30}
+def test_ini_reader():
+    reader = IniConfigReader('tests/config.ini', sections=['bitbucket.org', 'topsecret.server.com'])
+    assert reader._config.sections() == ['bitbucket.org', 'topsecret.server.com']
